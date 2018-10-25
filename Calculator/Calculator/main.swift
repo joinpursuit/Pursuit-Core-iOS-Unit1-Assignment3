2238917e501b365f69a7994969d81affbde16f2c
alexpaul <alexpaulnyc@gmail.com>
2018-10-25 15:25:20 -0400

    added xcode project

---
+++ Calculator/Calculator/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Alex Paul on 10/25/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
+  switch opString {
+  case "+":
+    return {x, y in x + y }
+  case "-":
+    return {x, y in x - y }
+  case "*":
+    return {x, y in x * y }
+  case "/":
+    return {x, y in x / y }
+  default:
+    return {x, y in x + y }
+  }
+}
