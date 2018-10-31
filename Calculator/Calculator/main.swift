110645e0f2a665464ea1459ceccc7d4a2d505833
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2018-10-31 16:34:28 -0400

    have three functions in their base

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -41,6 +41,9 @@ while validOperation {
     let userOperation = readLine() ?? "not a value"
     
     let componentOfOperation = userOperation.components(separatedBy: " ")
+    print(componentOfOperation)
+    
+    //if you are not giving me digit, operator, digit - three elements not getting in
     
     if let firstNum = Double(componentOfOperation[componentOfOperation.startIndex]){
         num1 = firstNum
@@ -62,3 +65,48 @@ while validOperation {
     print(mathStuffFactory(opString: componentOfOperation[1])(num1, num2)) // proof for fatal error
     
 }
+
+//filter function
+func myFilter(inputArray: [Int], filter: (Int) -> Bool) -> [Int] {
+    var resultArray = [Int]()
+    
+    for num in inputArray{
+        if filter(num){
+            resultArray.append(num)
+        }
+    }
+    return resultArray
+}
+
+
+let x = [1,2,1,3,4,1,2]
+let y = 3
+print(myFilter(inputArray: x){ y > $0 ? true : false})
+
+//map function
+func myMap(arrayOfNums: [Int], mapClosure: (Int) -> Int) -> [Int]{
+    var emptyBox = [Int]()
+    for num in arrayOfNums{
+        emptyBox.append(mapClosure(num))
+    }
+    return emptyBox
+}
+let inputUser = 2
+let r = [2,3,4,-5]
+print(myMap(arrayOfNums: r){inputUser * $0})
+
+
+//reduce function
+func myReduce(arrOfNum: [Int], user: Int, reduceClosure: (Int) -> Int) -> Int{
+    var tempResult = user
+    for num in arrOfNum{
+        tempResult += reduceClosure(num)
+    }
+    
+    return tempResult
+}
+
+let f = [2,1,3,3,1,2]//12 + 3 = 15
+let userInput = -3
+
+print(myReduce(arrOfNum: f, user: userInput){$0})
