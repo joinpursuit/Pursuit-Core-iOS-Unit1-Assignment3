f8f48cf06486f2a9f28e620031538e685767d9a3
Cameron Rivera <cameronrivera@Camerons-MacBook-Pro.local>
2019-10-11 14:46:22 -0400

    Attempted to implement mathStuffFactory

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+func isOperator(_ operation: String) -> Bool {
+    if(operation != "+" || operation != "-" || operation != "*" || operation != "/"){
+        return false
+    }else {
+        return true
+    }
+}
+
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
   switch opString {
   case "+":
@@ -22,3 +30,34 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     return {x, y in x + y }
   }
 }
+
+var operationsMessage = """
+    Select an operation from the following list:
+    Type \"+\" to perform addition.
+    Type \"-\" to perform subtraction.
+    Type \"*\" to perform multiplication.
+    Type \"\\" to perform division.
+"""
+
+var operationChoice: String = ""
+var operationClosure: (Double,Double) -> (Double)
+
+print(operationsMessage)
+operationChoice = readLine() ?? ""
+
+while (operationChoice.count != 1 || !isOperator(operationChoice)){
+    if(operationChoice.count != 1){
+        print("Not a valid operation.")
+        print(operationsMessage)
+        operationChoice = readLine() ?? ""
+    }else if (!isOperator(operationChoice)){
+        print("Not a valid operation.")
+        print(operationsMessage)
+        operationChoice = readLine() ?? ""
+    }
+}
+
+print(isOperator(operationChoice))
+
+operationClosure = mathStuffFactory(opString: operationChoice)
+print(operationClosure(10,5))
