4f01c333932ebce41df227992783d28f3f8d9fcc
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-10-14 16:53:50 -0400

    FInished part 1 of the lab, still need to give a prompt for users.

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -9,20 +9,48 @@
 import Foundation
 
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+    switch opString {
+    case "+":
+        return {x, y in x + y }
+    case "-":
+        return {x, y in x - y }
+    case "*":
+        return {x, y in x * y }
+    case "/":
+        return {x, y in x / y }
+    default:
+        return {x, y in x + y }
+    }
 }
+let operationsSet: Set<String.SubSequence> = ["+","-","/","*"]
+var exitis = false
+repeat {
+    let answer = Array((readLine() ?? "").split(separator: " "))
+    guard answer.count >= 3 else {
+        print("Please input a calculation.")
+        continue
+    }
+    
+    let x = Double(answer[0]) ?? -1
+    let y = Double(answer[2]) ?? -1
+    let operation = String(answer[1])
+    
+    if operationsSet.contains(answer[1]) {
+        print(mathStuffFactory(opString: operation)(x,y))
+    } else if answer[1] == "?" {
+        let randomOp = String(operationsSet.randomElement() ?? "+")
+        print(mathStuffFactory(opString: randomOp)(x,y))
+        print("Guess the operation.")
+        let randomAns = readLine() ?? ""
+        if randomAns == randomOp {
+            print("Correct")
+        } else {
+            print("Wrong")
+        }
+    } else {
+        print("Please input a valid operation.")
+    }
+    print(answer)
+} while exitis == false
 
-mathStuffFactory(opString: "+") {
-    (3, 4)
-}
+// add more guard statements.
