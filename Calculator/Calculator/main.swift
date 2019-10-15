5cf8b7cfa6a85126d344457e7567a646ee8deaad
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-15 17:15:09 -0400

    coded some parts

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -23,8 +23,30 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
   }
 }
 
+//let result = closureOperation(45, 5)
+print("Welcome! Please enter an equation. Example: 5 + 2 ")
+var userInput = readLine() ?? "0"
+var seperator = userInput.components(separatedBy: " ")
 let closureOperation = mathStuffFactory(opString: "+")
+let result = closureOperation(Double(seperator[0]) ?? 0.0 , Double(seperator[2]) ?? 0.0)
 
-let result = closureOperation(45, 5)
+
 
 print("result of operation is: \(result)")
+//result
+func str (in input: [String]) -> Double {
+
+    if userInput.count == 3 {
+        return result
+    }
+    else{
+        userInput = readLine() ?? "0"
+        print("Please enter an equation. Example: 7 + 2")
+    }
+    return 0.0
+}
+
+
+
+
+
