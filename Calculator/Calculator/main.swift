4e96d506f1727d2fd33a4356fc3115a3d8ee3b0e
Tsering Lama <tsenyk@Tserings-MacBook-Pro.local>
2019-10-15 17:13:07 -0400

    got the basics down, working on the errors

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -23,7 +23,37 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {      // 
   }
 }
 
-let operationClosure = mathStuffFactory(opString: "+")
-let result = operationClosure(45, 5)
-print(result)
+print("Hello welcome to my calculator")
+print()
+
+
+
+var userInput = readLine() ?? "a"
+//var userInputArr = userInput.components(separatedBy: " ")
+
+//let operationClosure = mathStuffFactory(opString: userInputArr[1])
+//let result = operationClosure( Double(userInputArr[0]) ?? 0.0 , Double(userInputArr[2]) ?? 0.0)
+
+
+
+// write a function that takes a string and returns a double
+// sepearte the string into an array
+func validCount(in str: String) -> Double {
+    var userInputArr = userInput.components(separatedBy: " ")
+    if userInputArr.count == 3 {
+        let operationClosure = mathStuffFactory(opString: userInputArr[1])
+        let result = operationClosure( Double(userInputArr[0]) ?? 0.0 , Double(userInputArr[2]) ?? 0.0)
+        return result
+    }
+    else {
+    print("Type in a number followed by an operation followed by a number.")
+        userInput = readLine() ?? "a"
+    }
+    return 0.0
+}
+
+
+
+
+
 
