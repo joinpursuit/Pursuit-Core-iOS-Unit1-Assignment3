400a0ff6feaa34016b1fdfd343141440f094150f
brendon cecilio <brendoncecilio@Brendons-MacBook-Pro.local>
2019-10-15 14:12:37 -0400

    intial syntax added

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -28,4 +28,22 @@ let closureOperation = mathStuffFactory(opString: "+")
 
 let result = closureOperation(45, 5)
 
+// ASK THE USER WHETHER THEY WANT A PROBLEM LVL 1 or LVL 2
+
+print("Enter a type of calculation, 1 (regular) or 2 (higher order)")
+let userResponse = readLine()?.lowercased() ?? ""
+
+func questionLevel(level: Int) -> (String) {
+    switch level {
+    case 1:
+        sleep(1)
+        print("Enter your operation. Ex: 5 * 2")
+    case 2:
+        sleep(1)
+        print("Enter your operation. Ex: 1,2,3,6,4,3,6,8,9,4 by < 5")
+    default:
+        print("Please enter a valid response! 🤬")
+    }
+}
+
 print("result of operation is \(result)")
