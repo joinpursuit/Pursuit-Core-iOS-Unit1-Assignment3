60561f13dc86faaa930a7540430a6aac73a6bd23
Ian Bailey <iankbailey2@gmail.com>
2019-10-16 12:34:11 -0400

    basic calculator done

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
+var operators = ["+", "-", "*","/"]
+
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
+    
   switch opString {
   case "+":
     return {x, y in x + y }
@@ -22,9 +25,56 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     return {x, y in x + y }
   }
 }
+func userInput() {
+    let userOperation = readLine()?.components(separatedBy: " ")
+    let x = Double(userOperation?[0] ?? "1.0")
+    let y = Double(userOperation?[2] ?? "1.0")
+    var operation = userOperation?[1] ?? " "
+    if operation == "?" {
+        operation = operators.randomElement() ?? "+"
+        var closureOperation = mathStuffFactory(opString: operation)
+        let randomValue = closureOperation(x ?? 1.0, y ?? 1.0)
+    } else  if operation != operators.description {
+        print("enter a valid operator")
+    } else {
+    let closureOperation = mathStuffFactory(opString: operation)
+    let result = closureOperation(x ?? 1.0, y ?? 1.0)
+    print(result)
+}
+}
+
+func randomOperator() {
+    
+}
+
+
+
+
+print("Welcome to the calculator")
+print("enter 1 for regular function and 2 for high order functions")
+var modeSelect = readLine()?.lowercased() ?? " "
+switch modeSelect {
+case "1":
+    print("enter a valid operation e.g x * y")
+    userInput()
+    
+case "2":
+    break
+    
+default :
+    break
+}
+    
+
+
+
+
+
 
 let closureOperation = mathStuffFactory(opString: "+")
 let result = closureOperation(45, 5)
 
-print("result of operation is \(result)")
+
+
+
 
