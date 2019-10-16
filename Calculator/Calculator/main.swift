f283dbd4e87e303e7a575a2c28a99d8a443e137b
Ian Bailey <iankbailey2@gmail.com>
2019-10-16 19:52:08 -0400

    guessing game complete

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-var operators = ["+", "-", "*","/"]
+var operators = ["+", "-", "*", "/"]
+
+
+
 
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     
@@ -25,31 +28,63 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
     return {x, y in x + y }
   }
 }
+
 func userInput() {
-    let userOperation = readLine()?.components(separatedBy: " ")
-    let x = Double(userOperation?[0] ?? "1.0")
-    let y = Double(userOperation?[2] ?? "1.0")
-    var operation = userOperation?[1] ?? " "
+    let userOperation = readLine()?.components(separatedBy: " ") ?? [" "]
+    let x = Double(userOperation[0])
+    let y = Double(userOperation[2])
+    let operation = userOperation[1]
+    var mathResult = 0.0
     if operation == "?" {
-        operation = operators.randomElement() ?? "+"
-        var closureOperation = mathStuffFactory(opString: operation)
-        let randomValue = closureOperation(x ?? 1.0, y ?? 1.0)
-    } else  if operation != operators.description {
-        print("enter a valid operator")
+    guard let x = x, let y = y else {
+            print("Invalid inputs")
+            return
+        }
+        mathResult = randomOperator(x: x, y: y, operatorSign: operation)
+        print("\(x) \(operation) \(y) = \(mathResult)")
+
+    } else if !operators.contains(operation) {
+
     } else {
     let closureOperation = mathStuffFactory(opString: operation)
-    let result = closureOperation(x ?? 1.0, y ?? 1.0)
+    let mathResult = closureOperation(x ?? 1.0, y ?? 1.0)
+   print("\(String(describing: x)) \(operation) \(String(describing: y)) = \(mathResult)")
+    }
+}
+
+func randomOperator(x: Double, y: Double, operatorSign: String) -> Double{
+    var result = 0.0
+    guard let randomOperator = operators.randomElement() else {
+        return 0.0
+    }
+     let closureOperation = mathStuffFactory(opString: randomOperator)
+     result = closureOperation(x, y)
     print(result)
-}
-}
-
-func randomOperator() {
+    guessingGame(operation: randomOperator)
     
+    
+    return result
 }
 
+func guessingGame(operation: String) {
+    guard let userGuessSign = readLine() else {
+        print("invalid input")
+        return
+    }
+    
+    guard operators.contains(userGuessSign) else {
+        print("This is not a valid mathematical operator")
+        return
+    }
+    
+    if userGuessSign == operation {
+        print("You got it write")
+    } else {
+        print("Not the right answer")
+    }
+}
 
-
-
+//calculator loop:
 print("Welcome to the calculator")
 print("enter 1 for regular function and 2 for high order functions")
 var modeSelect = readLine()?.lowercased() ?? " "
