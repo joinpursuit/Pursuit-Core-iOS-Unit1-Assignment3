cf2fafa58c299c116e269dd5b3aaaebb7f78e82c
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-17 15:59:44 -0400

    updated code

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -9,44 +9,92 @@
 import Foundation
 
 func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+    switch opString {
+    case "+":
+        return {x, y in x + y }
+    case "-":
+        return {x, y in x - y }
+    case "*":
+        return {x, y in x * y }
+    case "/":
+        return {x, y in x / y }
+    default:
+        return {x, y in x + y }
+    }
 }
 
 //let result = closureOperation(45, 5)
-print("Welcome! Please enter an equation. Example: 5 + 2 ")
-var userInput = readLine() ?? "0"
-var seperator = userInput.components(separatedBy: " ")
-let closureOperation = mathStuffFactory(opString: "+")
-let result = closureOperation(Double(seperator[0]) ?? 0.0 , Double(seperator[2]) ?? 0.0)
 
+print("""
+Welcome to the calculator.
+Please enter "1" for normal operations.
+Enter "2" for higher order operations.
+""")
 
+var userChoice = readLine()
+var decision = Int(userChoice!) ?? 1
+decision = Int(userChoice!) ?? 2
 
-print("result of operation is: \(result)")
-//result
-func str (in input: [String]) -> Double {
-
-    if userInput.count == 3 {
+if decision == 1{
+    print("Please enter an equation. Example: 5 + 2 ")
+    var userInput = readLine() ?? "0"
+   // var seperator = userInput.components(separatedBy: " ")
+    //var closureOperation = mathStuffFactory(opString: seperator[1])
+    //var result = closureOperation(Double(seperator[0]) ?? 0.0 , Double(seperator[2]) ?? 0.0)
+    //let bool = true
+    
+    
+    //print("result of operation is: \(result)")
+    //result
+    
+    
+    func str(in input: String) -> Double {
+        let seperator = userInput.components(separatedBy: " ")
+        let opString = seperator[1]
+        let closureOperation = mathStuffFactory(opString: opString)
+        var result = 0.0
+        if seperator.count != 3 {
+            return result
+        } else {
+            result = closureOperation(Double(seperator[0]) ?? 0.0 , Double(seperator[2]) ?? 0.0)
+        }
+        
         return result
     }
-    else{
-        userInput = readLine() ?? "0"
-        print("Please enter an equation. Example: 7 + 2")
-    }
-    return 0.0
+    
+    let result = str(in: userInput)
+    print(result)
+    
+    //func str (in input: [String]) -> Double {
+    //    let opString: String = seperator[1].randomElement() ?? "+"
+    //    closureOperation = mathStuffFactory(opString: "")
+    //    if userInput.count != 3 {
+    //        return result
+    //    } else if seperator[1] == "?"{
+    //
+    //    }
+    //    else{
+    //        userInput = readLine() ?? "0"
+    //        print("Please enter an equation. Example: 7 + 2")
+    //    }
+    //    return 0.0
+    //}
+    
+    //print("""
+    //Please enter a suprise operator for a secret equation. e.g 6 ? 5
+    //""")
+    
+    //userInput = readLine() ?? "0"
+    //func randomOperator(userOperator: [String]) -> Double {
+    //    let random = seperator[1] == "?"
+    //    if userInput.count != 3 {
+    //        if {
+    //
+    //            return result
+    //        }
+    //    }
+    //    return 0.0
+    //}
 }
-
-
-
 
 
