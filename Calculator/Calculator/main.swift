3e3cd7dacba010d9c679590dea712e839d0b7a6f
Eddieoneill <ejosephoneill@gmail.com>
2019-10-17 10:30:02 -0400

    cleaned up the code for splitting method

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -45,20 +45,28 @@ func myFilter(inputArray: [Int], filter: Int, filterType: String) -> [Int] {
     return filteredArray
 }
 
-func myMaps(inputArray numList: [Int], inputInt mulBy: Int) -> [Int] {
+func myMaps(inputArray numList: [Int], inputInt: Int, mapType: String) -> [Int] {
     var result = [Int]()
     
-    for num in numList {
-        result.append(num * mulBy)
+    if mapType == "*" {
+        for num in numList {
+            result.append(num * inputInt)
+        }
+    } else if mapType == "/" {
+        for num in numList {
+            result.append(num / inputInt)
+        }
     }
+    
     
     return result
 }
 
 func myReduce(inputArray: [Int], startingNumber: Int, reduceType: String) -> Int {
     var result = Int()
-    let inputType = ["-", "+"]
+    let inputType = ["-", "+", "*"]
     result = startingNumber
+    print(result)
     
     if reduceType == inputType[0] {
         for num in inputArray {
@@ -68,9 +76,34 @@ func myReduce(inputArray: [Int], startingNumber: Int, reduceType: String) -> Int
         for num in inputArray {
             result += num
         }
+    } else if reduceType == inputType[2] {
+        for num in inputArray {
+            result *= num
+        }
     }
     
     return result
+}
+
+func inputStrSepration(input: String) -> [String] {
+    var userInputArr = [String]()
+    var word = ""
+    
+    for (index, str) in input.lowercased().enumerated() {
+        if index == 0 && (str == " " || str == ",") {
+            continue
+        } else if str == " " || str == "," {
+            userInputArr.append(word)
+            word = ""
+        } else {
+            word += String(str)
+        }
+        
+        if index == input.count - 1 {
+            userInputArr.append(word)
+        }
+    }
+    return userInputArr
 }
 
 while stop == false {
@@ -80,31 +113,36 @@ while stop == false {
     var calculator = ""
     var calculatorNum: [Double] = []
     var numberList: [Int] = []
-    var inputNumber = Int()
-    var filterType = ""
-    var reduceType = ""
-    let splitInput1 = userInput.split(separator: " ")
-    let wordCheck = splitInput1[0]
+    var actionType = ""
+    var splitInputArr = inputStrSepration(input: userInput)
+    var wordCheck = Set(splitInputArr)
+    var inputNumber = Int(splitInputArr[splitInputArr.count - 1]) ?? 0
     
-    func stringSepration() {
-        var processingOfFilter: [Substring.SubSequence] = []
-        inputNumber = Int(splitInput1[splitInput1.count - 1]) ?? 0
-        for element in splitInput1 where element != splitInput1[splitInput1.count - 1] {
-            processingOfFilter = element.split(separator: ",")
-            for value in processingOfFilter {
-                if Int(value) != nil {
-                    numberList.append(Int(value)!)
-                } else if (value == "<" || value == ">") {
-                    filterType = String(value)
-                } else if (value == "+" || value == "-") {
-                    reduceType = String(value)
-                }
-            }
+    for (index, value) in splitInputArr.enumerated() where index != splitInputArr.count - 1 {
+        if wordCheck.contains("filter") && wordCheck.contains("<") {
+            actionType = "<"
+        } else if wordCheck.contains("filter") && wordCheck.contains(">") {
+            actionType = ">"
+        } else if wordCheck.contains("reduce") && wordCheck.contains("+") {
+            actionType = "+"
+        } else if wordCheck.contains("reduce") && wordCheck.contains("-") {
+            actionType = "-"
+        } else if wordCheck.contains("reduce") && wordCheck.contains("*") {
+            actionType = "*"
+        } else if wordCheck.contains("map") && wordCheck.contains("*") {
+            actionType = "*"
+        } else if wordCheck.contains("map") && wordCheck.contains("/") {
+            actionType = "/"
+        }
+        
+        if Int(value) != nil {
+            numberList.append(Int(value)!)
         }
     }
     
+    
     func calculation() {
-        for element in splitInput1 {
+        for element in splitInputArr {
             if Double(element) != nil {
                 calculatorNum.append(Double(element) ?? 0)
             } else if calculationList.contains(String(element)) {
@@ -115,34 +153,29 @@ while stop == false {
         }
     }
     
-    switch wordCheck {
-    case "filter":
-        stringSepration()
-        
-        if filterType.count > 0 {
-            let result = myFilter(inputArray: numberList, filter: inputNumber, filterType: filterType)
-            print("here is the filted list: \(result)")
+    if wordCheck.contains("filter") {
+        if actionType.count > 0 {
+            let result = myFilter(inputArray: numberList, filter: inputNumber, filterType: actionType)
+            print("here is the filtered list: \(result)")
         } else {
             print("I got into the filter but wasn't sure what to do with it")
         }
-    case "reduce":
-        stringSepration()
-        if numberList.count != 0 && reduceType.count > 0 {
-            let result = myReduce(inputArray: numberList, startingNumber: inputNumber, reduceType: reduceType)
-            print("here is the reduced list: \(result)")
+    } else if wordCheck.contains("reduce") {
+        if numberList.count != 0 && actionType.count > 0 {
+            let result = myReduce(inputArray: numberList, startingNumber: inputNumber, reduceType: actionType)
+            print("here is the reduced result is: \(result)")
         } else {
             print("I got into the reduce but wasn't sure what to do with it")
         }
-    case "map":
-        stringSepration()
-        if numberList.count != 0 && inputNumber != 0 {
+    } else if wordCheck.contains("map") {
+        if numberList.count != 0 && actionType.count > 0 {
             print(numberList)
-            let result = myMaps(inputArray: numberList, inputInt: inputNumber)
+            let result = myMaps(inputArray: numberList, inputInt: inputNumber, mapType: actionType)
             print("here is the mapped list: \(result)")
         } else {
             print("I got into the map but wasn't sure what to do with it")
         }
-    default:
+    } else {
         calculation()
         if calculatorNum.count == 2 {
             let calculationType = mathStuffFactory(opString: calculator)
@@ -157,11 +190,17 @@ while stop == false {
     
     print("if you want to do another calculation please enter 'yes'")
     let more = readLine()?.lowercased() ?? "no"
-    
+
     if more != "yes" {
         stop = true
     }
+    
+    splitInputArr.removeAll()
+    
 }
+
+
+
 
 
 
