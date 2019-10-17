80545466c9dad935decf1f32b0a138b2c2bcc835
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-10-17 15:22:56 -0400

    Added Random Game

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -31,40 +31,21 @@ var operations: ([String: (Double, Double) -> Double]) = ["+": { $0 + $1 },
 "?": {
     
         let randomOperand = Int.random(in: 0...3)
-        var operandKey = ""
         if randomOperand == 0   {
-            operandKey = "+"
             return $0 + $1
         }
         else if randomOperand == 1  {
-            operandKey = "-"
             return $0 - $1
         }
         else if randomOperand == 2  {
-            operandKey = "*"
             return $0 * $1
         }
         else    {
-            operandKey = "/"
             return $0 / $1
         }
     }
 ]
 
-/*
-func guessOperator(operandQ : String, operand : String)    {
-    var operatorGuess = ""
-    if operandQ == "?"   {
-        print("Guess operator")
-        operatorGuess = readLine() ?? ""
-    }
-    repeat{ if operatorGuess == operand {
-        print("Correctomundo")
-    }
-    else    {print("Try Again")}
-    } while operatorGuess != operand
-}
-*/
 
 // prompt user for entry
 print("Enter your operation, e.g 5 + 3")
@@ -76,7 +57,7 @@ let userEntry : String = readLine() ?? ""
 // split user entry into array
 var entryParts = userEntry.components(separatedBy: " ")
 
-print(entryParts)
+//print(entryParts)
 
 // variable for each element
 let firstTermArray = Double(entryParts[0])
@@ -90,7 +71,7 @@ let firstTermTuple = operationString.0
 let secondTermTuple = operationString.1
 let operandTuple = operationString.2
 
-print(operationString)
+//print(operationString)
 
 // function, takes  paramaters of sparsed string to determine key for dictionary of closures
 // and perform according operation closure
@@ -98,16 +79,45 @@ print(operationString)
 func basicCalculation(operandEntry : String?, termOne : Double?, termTwo : Double?, operationsClosure: ([String: (Double, Double) -> Double])) -> Double {
     
     var result = 0.0
+    var rightOperand = ""
+    var operandGuess = ""
     if let operate = operations[operandEntry ?? ""]    {
         result = operate(termOne ?? 0, termTwo ?? 0)
     }
     
+    print("Result: \(result)")
+    
+    if operandEntry == "?"  {
+        print("Guess the operator used for your result, keep trying if wrong.")
+        repeat  {
+            operandGuess = readLine() ?? ""
+        if result == (termOne ?? 0) + (termTwo ?? 0)    {
+            rightOperand = "+"
+            }
+        else if result == (termOne ?? 0) - (termTwo ?? 0)    {
+            rightOperand = "-"
+        }
+        else if result == (termOne ?? 0) * (termTwo ?? 0)    {
+            rightOperand = "*"
+        }
+        else if result == (termOne ?? 0) / (termTwo ?? 0)    {
+            rightOperand = "/"
+        }
+        if operandGuess == rightOperand {
+            print("Correctomundo!")
+        }
+        else    {
+            print("Try again.")
+        }
+        } while operandGuess != rightOperand
+    //add random game in here or outside as closure function and use  in here
+    }
     return result
 }
 
 
 // call function use tuple parts for necessary arguments for function into dictionary closure
-print(basicCalculation(operandEntry: operandTuple, termOne : firstTermTuple, termTwo: secondTermTuple, operationsClosure: operations))
+basicCalculation(operandEntry: operandTuple, termOne : firstTermTuple, termTwo: secondTermTuple, operationsClosure: operations)
 
 
 /*
